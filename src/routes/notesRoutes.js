adf186b97b43584e3ac83e283776ac1ec9e6d593
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-12-07 01:59:21 +0530

    Implement getNoteById function and enhance updateNote and deleteNote error handling in notes controller

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,9 +3,12 @@ import { getAllNotes } from "../controllers/notesController.js";
 import { createNote } from "../controllers/notesController.js";
 import { updateNote } from "../controllers/notesController.js";
 import { deleteNote } from "../controllers/notesController.js";
+import { getNoteById } from "../controllers/notesController.js";
 const router = express.Router();
 
 router.get("/", getAllNotes);
+
+router.get("/:id", getNoteById);
 
 router.post("/", createNote);
 
