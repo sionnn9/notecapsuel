140663e63a924b5682a02717953336feded94c3c
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-12-17 18:10:26 +0530

    Add rate limiting functionality using Upstash and integrate into server middleware

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,20 @@
+import e from "express";
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-limit-key"); //success needs stupid brackets
+    //my-limit-key is supposed to have userid which can be implimented when we use auth which is not done here
+    if (!success) {
+      return res
+        .status(429)
+        .json({ Message: "Too many requests. Please try again later." });
+    }
+    next(); //put next inside try block to ensure it only gets called if rate limit check passes
+  } catch (err) {
+    console.error("Rate limiter error:", err);
+    next(err);
+  }
+};
+
+export default rateLimiter;
--- src/server.js
+++ src/server.js
@@ -3,15 +3,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.Port || 5001;
-connectDB();
 
+//middleware
 app.use(express.json());
+
+app.use(rateLimiter);
+
+app.use((req, res, next) => {
+  console.log(`req method is ${req.method} and req url is ${req.url}`);
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port 5001: http://localhost:5001");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port 5001: http://localhost:5001");
+  });
 });
