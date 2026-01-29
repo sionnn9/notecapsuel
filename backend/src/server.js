4247c8697a768d4759ef7a0f474f99a2afd853d4
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-01-29 20:44:46 +0530

    fix: correct server.js port reference and update package.json scripts for consistency

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,16 +8,22 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.Port || 5001;
+const PORT = process.env.PORT || 5001;
 
 //middleware
 // this should be before app.use(express.json()) to handle preflight requests because of CORS
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-app-name.vercel.app", // replace after deploy
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000", // frontend url port
-    credentials: true, // allow cookies to be sent
-  })
+    origin: allowedOrigins,
+    credentials: true,
+  }),
 );
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -30,6 +36,6 @@ app.use("/api/notes", notesRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server is running on port 5001: http://localhost:5001");
+    console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
   });
 });
