4703638f1be9e26671338e58459a3cebf8595068
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-02-13 20:51:54 +0530

    improved register function added cehcking if user exits

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,14 +2,30 @@ import user from "../models/user.js";
 
 export async function register(req, res) {
   try {
-    const newUser = await user.create(req.body);
-    const data = res.status(201).json({ Message: "user created successfully" });
-    if (!newUser) {
-      return res.status(400).json({ Message: "alr exists" });
+    const { name, email, password } = req.body;
+
+    // check if user exists
+    const existingUser = await user.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    const newUser = await user.create({
+      name,
+      email,
+      password, // (hash this if using bcrypt)
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+      userId: newUser._id,
+    });
   } catch (error) {
-    console.error("error in register", error);
-    res.status(500).json({ Message: "server error" });
+    console.error("REGISTER ERROR:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
   }
 }
 
