8388d8606cfaf79cb090b5c0f7be37823756605c
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-02-13 19:50:11 +0530

    comnt

--- frontend/note-cap/app/signup/page.tsx
+++ frontend/note-cap/app/signup/page.tsx
@@ -21,6 +21,7 @@ const SignupPage = () => {
       console.error("Signup error:", error);
     }
   };
+
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
