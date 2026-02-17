4caa02ee02a4c3751a01b8c148d71c2c214419e3
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2026-02-17 21:05:31 +0530

    fixed i think everything works now just checking and small things to change rest works

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -15,7 +15,7 @@ export async function getNoteById(req, res) {
   try {
     const note = await Note.findOne({
       _id: req.params.id,
-      user: req.user.id, // ensures note belongs to logged-in user
+      user: req.user.userId, // ensures note belongs to logged-in user
     });
     if (!note) {
       return res.status(404).json({ Message: "note not found" });
